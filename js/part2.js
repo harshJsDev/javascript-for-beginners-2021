843de821c47e38a6b120021b8fb495c0111e9dd5
harsh kumar <harshkumar2009@gmail.com>
2021-02-10 18:23:35 +0530

    javascript part 2

---
+++ js/part2.js
@@ -0,0 +1,17 @@
+// variables in javascript
+// let variablesName = "data";
+// alert(3 + 2);
+let firstNumber = 11;
+let secondNumber = 64;
+let result = firstNumber + secondNumber;
+console.log("the sum of two numbers = " + result);
+
+let = 16
+
+// string bol decimal undefine number symbol
+
+let fname = "harsh Kumar";
+let bolvalue = true;
+let companyName = undefined;
+let pieValue = 3.14;
+console.log("name is "+ fname +" "+ companyName +" " + pieValue);
