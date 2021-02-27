dc0f7121acc1f9e92a68c3ff071fabc6d9981448
harsh kumar <harshkumar2009@gmail.com>
2021-02-28 04:20:52 +0530

    part 4

---
+++ js/part4.js
@@ -0,0 +1,55 @@
+// part4
+// Basic arithmetic operations
+
+// + - * / () % mod ** power
+
+// addition
+
+let number1 = 6;
+let number2 = 9;
+let sum = number1 + number2;
+console.log(`the sum of two numbers , ${number1} and ${number2} is ${sum}`);
+
+// subtraction
+let number3 = 15;
+let number4 = 3;
+let subtraction = number3 - number4;
+console.log(`the subtraction of two numbers , ${number3} and ${number4} is ${subtraction}`);
+
+// multiplication
+let number5 = 5;
+let number6 = 9;
+let multiplication = number5 * number6;
+console.log(`the multiplication of two numbers , ${number5} and ${number6} is ${multiplication}`);
+
+
+// division
+let number7 = 12;
+let number8 = 3;
+let division = number7 / number8;
+console.log(`the division of two numbers , ${number7} and ${number8} is ${division}`);
+
+// mod
+let number9 = 13;
+let number10 = 3;
+let mod = number9 % number10;
+console.log(`the mod of two numbers , ${number9} and ${number10} is ${mod}`);
+
+// mod
+let number11 = 9 ** 3;
+
+
+console.log(`the exponential value  of a  number  is, ${number11} `);
+
+/*
+B = Brackets ()
+O = Of or Exponent
+D = Divide
+M = Multi
+A = Add
+S = Subtract
+*/
+
+let expression = (14 + 3) - 5 * 2;
+console.log(`The result of the expression is ${expression}`);
+
