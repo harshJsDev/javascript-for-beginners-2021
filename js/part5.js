609be01a00bffdf9e38573d30fa4866627cd1adf
harsh kumar <harshkumar2009@gmail.com>
2021-04-15 20:44:56 +0530

    js tutorial part 5,6,7

---
+++ js/part5.js
@@ -0,0 +1,2 @@
+let a = 25;
+(a == 5) ? console.log(typeof(true)) : console.log(typeof(false));
---
+++ js/part6.js
@@ -0,0 +1,22 @@
+// String is text data Like this
+// It can be long......
+// it can a single character 
+let examplestring = "hello world";
+console.log(examplestring);
+
+let examplestring2 = 'code Fusion';
+console.log(examplestring2);
+
+let examplestring3 = `Harsh kumar`;
+console.log(examplestring3);
+// let examplestring4 = 'Wrong syntax";
+
+let stringLength = "Hi how are you?";
+let result = stringLength.length;
+console.log(result);
+
+let firstName = "Code ";
+let secondName = "Fusion";
+
+let output = firstName.concat(secondName);
+console.log(output);
