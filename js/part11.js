412dc97913888e47374cbf3db0904929b1e9eeeb
harsh kumar <harshkumar2009@gmail.com>
2021-06-04 10:13:26 +0530

    switch case

---
+++ js/part11.js
@@ -0,0 +1,71 @@
+// let month = 52;
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//   // break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   case 5:
+//     console.log("May");
+//     break;
+//   case 6:
+//     console.log("June");
+//     break;
+//   case 7:
+//     console.log("July");
+//     break;
+//   case 8:
+//     console.log("August");
+//     break;
+//   case 9:
+//     console.log("September");
+//     break;
+//   case 10:
+//     console.log("October");
+//     break;
+//   case 11:
+//     console.log("November");
+//     break;
+//   case 12:
+//     console.log("December");
+//     break;
+//   default:
+//     console.log("This is not a Month");
+//     break;
+// }
+
+let day = "tuefg";
+switch (day) {
+  case "mon":
+    console.log("Monday");
+    break;
+  case "tue":
+    console.log("Tuesday");
+    break;
+  case "wed":
+    console.log("Wednesday");
+    break;
+  case "thur":
+    console.log("Thursday");
+    break;
+  case "fri":
+    console.log("Friday");
+    break;
+  case "sat":
+    console.log("Saturday");
+    break;
+  case "sun":
+    console.log("Sunday");
+    break;
+  
+  default:
+    console.log(`${day} abbreviation is not correct. Please check Your spelling`);
+    break;
+}
