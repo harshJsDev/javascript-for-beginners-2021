b4feb0777e74068f1d76a0093dfb2262d45acbe8
harsh kumar <harshkumar2009@gmail.com>
2021-06-10 12:02:32 +0530

    part 12, 13

---
+++ js/part12.js
@@ -0,0 +1,15 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+
+
+// for (let initialValue = 1; initialValue <= 100; initialValue++){
+//     console.log(initialValue);
+// }
+
+for (let initialValue = 1; initialValue <= 100; initialValue++){
+    if (initialValue % 2 === 1) {
+        console.log(`${initialValue} is a even number`);
+    }
+}
---
+++ js/part13.js
@@ -0,0 +1,10 @@
+// let initialValue = 1;
+// while (initialValue <= 10) {
+//     console.log(initialValue);
+//     initialValue++;
+// }
+let initialValue = 10;
+while (initialValue <= 10) {
+    initialValue++;
+    console.log(initialValue);
+}
