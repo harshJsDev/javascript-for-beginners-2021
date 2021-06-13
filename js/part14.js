35ea44ef04c0bbe68eaffbd74f812495b58b7146
harsh kumar <harshkumar2009@gmail.com>
2021-06-13 14:23:09 +0530

    part14
    do while loo[

---
+++ js/part14.js
@@ -0,0 +1,12 @@
+// do {
+//     // statement
+//     // increment/ decrement
+
+// } while (condition);
+
+let iterationValue = 1;
+do {
+    document.write(`<h1>${iterationValue}. I ❤️ Code Fusion</h1>`);
+    iterationValue++;
+}
+while (iterationValue <1);
