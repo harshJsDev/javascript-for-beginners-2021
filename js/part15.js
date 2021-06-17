fbfacc80458d20f20217c0be08a6ba94872ef15a
harsh kumar <harshkumar2009@gmail.com>
2021-06-17 20:06:58 +0530

    part15

---
+++ js/part15.js
@@ -0,0 +1,11 @@
+// let name = "Harsh kumar, code fusion";
+// console.log(name);
+
+// syntax
+// let ArrayName = ["value1", "value2"];
+let listOfAnimal = ["Lion", "Tiger", "Python", "Pug"];
+// console.log(listOfAnimal[2]);
+listOfAnimal.push("Eagle");
+listOfAnimal.pop();
+console.log(listOfAnimal);
+listOfAnimal.
