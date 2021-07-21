f54db7fd0301cf25a5ed590fdf4808516fbb2d38
harsh kumar <harshkumar2009@gmail.com>
2021-07-21 14:06:00 +0530

    part 17

---
+++ js/part17.js
@@ -0,0 +1,8 @@
+let fruitList = ["Apple", "banana"];
+let vegyList = ["Carrot", "Cabbage"];
+
+let newArray = fruitList.concat(vegyList);
+console.log(newArray);
+console.log(fruitList);
+console.log(vegyList);
+ 
