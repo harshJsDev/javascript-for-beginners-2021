16424711171c79ef8f34198e8b367327b3186a74
harsh kumar <harshkumar2009@gmail.com>
2021-08-14 09:00:25 +0530

    part 25

---
+++ js/part25.js
@@ -0,0 +1,39 @@
+
+// console.log(Math.floor(12.2));
+
+function whatIsYourName() {
+    console.log(`My Name is Harsh kumar`);
+    console.log(`You are watching code fusion.`);
+}
+// whatIsYourName();
+// console.log();
+// alert();
+
+// function addNumber() {
+//     let num1=5, num2=10, num3;
+//     num3 = num1 + num2;
+//     console.log(num3);   
+// }
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+// addNumber();
+
+function name(params) {
+    
+}
+
+function whatIsYourName(name) {
+    console.log(`My Name is ${name}`);
+    
+}
+
+
+whatIsYourName(123)
