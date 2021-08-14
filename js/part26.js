7b2a4172001a8b45b78b61f7c6578e73fa2d8bc5
harsh kumar <harshkumar2009@gmail.com>
2021-08-15 01:25:29 +0530

    part 26

---
+++ js/part26.js
@@ -0,0 +1,29 @@
+/* function whatIsYourName(name) {
+    return`My Name is ${name}`;
+    
+}
+let str = whatIsYourName("Code Fusion");
+console.log(str + " New line");
+
+function avg(number1, number2 ) {
+    let result = (number1 + number2) / 2;
+    return result;
+}
+
+console.log(avg(5, 9)); */
+
+// function div(number2, number1) {
+//     let result = (number1 / number2);
+//     return result;
+// }
+
+
+function div(number1, number2 ) {
+    let result = (number1 / number2);
+    let result1 = (number1 * number2);
+    
+    return [result,result1];
+    
+}
+
+console.log(div(6, 2));
