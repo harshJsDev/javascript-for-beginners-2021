b6dc0ab958b4e5da90f1449593f2a6d3e046dae6
harsh kumar <harshkumar2009@gmail.com>
2021-09-28 09:54:30 +0530

    part30

---
+++ js/part30.js
@@ -0,0 +1,25 @@
+// // regular Old syntax
+
+// function fullName(fName, lName) {
+//     return `${fName} ${lName}`;
+// }
+// console.log(fullName("Harsh", "Kumar"));
+
+
+// let fullName = (fName, lName) =>{
+//     return `${fName} ${lName}`;
+// }
+// console.log(fullName("Harsh", "Kumar"));
+
+// let fullName = (fName, lName) => `${fName} ${lName}`;
+
+
+// let addition = (number1, number2) => number1 + number2;
+// console.log(addition(5, 6));
+
+// let diceRoll = side => 1 + Math.floor(Math.random()*side)
+
+// console.log(diceRoll(6));
+
+let welcome = () => `Hello welcome to code fusion! Thank you for subscribing in advance`
+console.log(welcome());
