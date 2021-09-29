66970bd2efdff958ebf88ca140796d16438894a4
harsh kumar <harshkumar2009@gmail.com>
2021-09-29 17:59:22 +0530

    part31

---
+++ js/part31.js
@@ -0,0 +1,6 @@
+let divide = (number, dividedBy = 1) => number / dividedBy;
+
+console.log(divide(6, 3));
+let welcomeNewUser = ( message, username = "New User") => `${username}, ${message}`
+
+console.log(welcomeNewUser("Welcome to Code Fusion")); 
