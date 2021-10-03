7b67d6ccd97feb606d8e984a7568ed1070539c90
harsh kumar <harshkumar2009@gmail.com>
2021-10-03 12:47:29 +0530

    Part32

---
+++ js/part32.js
@@ -0,0 +1,6 @@
+let listGrocery = ["banana", "eggs", "milk", "apple"];
+// console.log(listGrocery[0]);
+
+listGrocery.forEach(function (value, index) {
+  console.log(`${index}: Value = ${value}`);
+});
